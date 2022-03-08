a4949d291086c984cd257d9a49d23820b47c6c50
dlwldbs369 <87183919+dlwldbs369@users.noreply.github.com>
2022-03-08 23:04:42 +0900

    delete myapp
    delete myapp

--- myapp/node_modules/rc-resize-observer/es/SingleObserver/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import type { ResizeObserverProps } from '..';
-export interface SingleObserverProps extends ResizeObserverProps {
-    children: React.ReactElement;
-}
-export default function SingleObserver(props: SingleObserverProps): JSX.Element;
